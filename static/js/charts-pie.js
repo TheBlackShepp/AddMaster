f1fd322b4d5e7296cd014abc04fb96fb7d065a85
TheBlackShepp <45878334+TheBlackShepp@users.noreply.github.com>
2020-12-25 18:25:21 +0100

    first commit

---
+++ static/js/charts-pie.js
@@ -0,0 +1,27 @@
+
+const pieConfig = {
+  type: 'doughnut',
+  data: {
+    datasets: [
+      {
+        data: [33, 33, 33],
+      
+        backgroundColor: ['#0694a2', '#1c64f2', '#7e3af2'],
+        label: 'Dataset 1',
+      },
+    ],
+    labels: ['Shoes', 'Shirts', 'Bags'],
+  },
+  options: {
+    responsive: true,
+    cutoutPercentage: 80,
+    
+    legend: {
+      display: false,
+    },
+  },
+}
+
+// change this to the id of your chart element in HMTL
+const pieCtx = document.getElementById('pie')
+window.myPie = new Chart(pieCtx, pieConfig)
