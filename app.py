814cdd6a212ece2c2579fc951a9a9d71a0b350b5
TheBlackShepp <45878334+TheBlackShepp@users.noreply.github.com>
2020-12-25 18:53:49 +0100

    cambio de __init__ a app

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, render_template, request
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('login.html')
+
+@app.route('/create-account')
+def createAccount():
+    return render_template('create-account.html')
+    
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=8080)  # This line is required to run Flask on repl.it
