68725a1afcbc7fd3e234e16ce13ced0fdbe6d049
Alvaro Guti <alvaro.gutierrez.hernandez97@gmail.com>
2021-01-12 18:32:39 +0100

    nuevos htmls

--- static/js/script.js
+++ static/js/script.js
@@ -3,6 +3,7 @@ const subpath = LOCATION.pathname.split('/')[LOCATION.pathname.split('/').length
 
 const URL_GET_LIST_PRODUCT = 'get_lista_productos'
 const URL_GET_LIST_CLIENT = 'get_lista_productos'
+const URL_GET_LIST_ORDER = 'get_lista_productos'
 
 
 const makeGet = async(url) => {
@@ -53,6 +54,13 @@ const makePost = async(url, datos) => {
             break;
     
         case 'pedidos':
+            makePost(URL_GET_LIST_ORDER, {})
+                .then(data => {
+                    console.log(data);
+                    let list = [{'name': 'Martina Casas'}, {'name': 'Marcos Martin'}]
+                    printListOrders(list)
+                })
+                .catch(err => console.error)
             break;
 
         case 'materias':
@@ -101,7 +109,6 @@ function printListProducts(listProducts){
     container.innerHTML = text;
 }
 
-
 // pintar clientes
 const makeRowClient = (client) => {
 
@@ -140,7 +147,7 @@ const makeRowClient = (client) => {
                 <button
                 class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
                 aria-label="Edit"
-                onclick="viewDetailsProduct('client')">
+                onclick="viewDetailsClient('client')">
                 <svg
                     class="w-5 h-5"
                     aria-hidden="true"
@@ -186,12 +193,119 @@ function printListClients(listClients){
     container.innerHTML = text;
 }
 
+// pintar pedidos
+const makeRowOrder = (order) => {
+
+    return `
+        <tr class="text-gray-700 dark:text-gray-400">
+            <td class="px-4 py-3">
+            <div class="flex items-center text-sm">
+                <!-- Avatar with inset shadow -->
+                <div
+                class="relative hidden w-8 h-8 mr-3 rounded-full md:block"
+                >
+                <img
+                    class="object-cover w-full h-full rounded-full"
+                    src="https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjE3Nzg0fQ"
+                    alt=""
+                    loading="lazy"
+                />
+                <div
+                    class="absolute inset-0 rounded-full shadow-inner"
+                    aria-hidden="true"
+                ></div>
+                </div>
+                <div>
+                <p class="font-semibold">Hans Burger</p>
+                <p class="text-xs text-gray-600 dark:text-gray-400">
+                    10x Developer
+                </p>
+                </div>
+            </div>
+            </td>
+            <td class="px-4 py-3 text-sm">
+            $ 863.45
+            </td>
+            <td class="px-4 py-3 text-xs">
+            <span
+                class="px-2 py-1 font-semibold leading-tight text-green-700 bg-green-100 rounded-full dark:bg-green-700 dark:text-green-100"
+            >
+                Approved
+            </span>
+            </td>
+            <td class="px-4 py-3 text-sm">
+            6/10/2020
+            </td>
+            <td class="px-4 py-3">
+            <div class="flex items-center space-x-4 text-sm">
+                <button
+                class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                aria-label="Edit"
+                onclick="viewDetailsOrder('order')"
+                >
+                <svg
+                    class="w-5 h-5"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                >
+                    <path
+                    d="M13.586 3.586a2 2 0 112.828 2.828l-.793.793-2.828-2.828.793-.793zM11.379 5.793L3 14.172V17h2.828l8.38-8.379-2.83-2.828z"
+                    ></path>
+                </svg>
+                </button>
+                <button
+                class="flex items-center justify-between px-2 py-2 text-sm font-medium leading-5 text-purple-600 rounded-lg dark:text-gray-400 focus:outline-none focus:shadow-outline-gray"
+                aria-label="Delete"
+                onclick="deleteOrder('order')"
+                >
+                <svg
+                    class="w-5 h-5"
+                    aria-hidden="true"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                >
+                    <path
+                    fill-rule="evenodd"
+                    d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
+                    clip-rule="evenodd"
+                    ></path>
+                </svg>
+                </button>
+            </div>
+            </td>
+        </tr>
+    `
+}
+function printListOrders(listOrders){
+    const container = document.getElementById('container-orders');
+
+    let text = ''
+    listOrders.forEach(order => {
+        text+= makeRowOrder(order);
+    })
+
+    container.innerHTML = text; 
+}
+
 // view details product
 function viewDetailsProduct(id){
     console.log(id)
-    const modal = document.getElementById('modal-full')
-    modal.style.transform= 'translateY(6.5%)'
+    showFullModal()
 }
+
+// view detail client
+function viewDetailsClient(id){
+    console.log(id)
+    showFullModal()
+}
+
+// view detail order
+function viewDetailsOrder(id){
+    console.log(id)
+    showFullModal()
+}
+
 
 
 // eliminar cliente
@@ -207,6 +321,27 @@ function deleteClient(id){
             )
             }
         })
+}
+
+// eliminar pedido
+function deleteOrder(id){
+
+    showModalDelete()
+        .then((result) => {
+            if (result.isConfirmed) {
+            Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+            )
+            }
+        })
+}
+
+// TODO show modal
+function showFullModal() {
+    const modal = document.getElementById('modal-full')
+    modal.style.transform= 'translateY(6.5%)'
 }
 
 // TODO close modal
